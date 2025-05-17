0423f729bc3a093a5ca7be73d0abea2b2bce339e
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-05-17 19:14:58 -0300

    feat: Decrease Node desired size

--- eks.tf
+++ eks.tf
@@ -32,8 +32,8 @@ module "eks" {
   eks_managed_node_groups = {
     amc-cluster-wg = {
       min_size       = 1
-      max_size       = 2
-      desired_size   = 2
+      max_size       = 1
+      desired_size   = 1
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
       security_groups = [aws_security_group.eks_sg.id]
