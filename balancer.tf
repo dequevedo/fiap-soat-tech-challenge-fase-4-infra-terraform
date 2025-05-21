3c8a97695b3e59972595df689cc9b911107d0a02
Renato Martins <70973620+RenatoMartinsXrd@users.noreply.github.com>
2025-05-21 02:10:52 -0300

    Update balancer.tf

--- balancer.tf
+++ balancer.tf
@@ -76,7 +76,7 @@ resource "aws_lb_target_group" "product_target_group" {
   health_check {
     interval = 30
     path     = "/actuator/health"
-    port     = 30080
+    port     = 8080
     protocol = "HTTP"
     timeout  = 5
     healthy_threshold   = 2
