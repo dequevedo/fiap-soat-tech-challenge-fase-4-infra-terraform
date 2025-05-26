3069033ae4c83afb027a38f40de42f3cf0ca4c27
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-05-25 21:12:23 -0300

    feat: Fix NLB and ingress

--- balancer.tf
+++ balancer.tf
@@ -1,7 +1,7 @@
-# Security Group para o NLB
-resource "aws_security_group" "product_nlb_sg" {
-  name        = "product-nlb-sg"
-  description = "Security group for the product service NLB"
+# Security Group para o NLB do Ingress
+resource "aws_security_group" "ingress_nlb_sg" {
+  name        = "ingress-nlb-sg"
+  description = "Security group for the Ingress NLB"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -19,76 +19,61 @@ resource "aws_security_group" "product_nlb_sg" {
   }
 
   tags = {
-    Name = "product-nlb-sg"
+    Name = "ingress-nlb-sg"
   }
 }
 
-## NLB para o Ingress Controller
-#resource "aws_lb" "product_nlb" {
-#  name               = "product-nlb"
-#  internal           = false
-#  load_balancer_type = "network"
-#  security_groups    = [aws_security_group.product_nlb_sg.id]
-#  subnets            = module.vpc.private_subnets
-#  enable_deletion_protection = false
-#
-#  tags = {
-#    Name = "product-nlb"
-#  }
-#}
-#
-## Target Group com protocolo TCP
-#resource "aws_lb_target_group" "product_target_group" {
-#  name        = "product-target-group"
-#  protocol    = "TCP"
-#  port        = 30080
-#  vpc_id      = module.vpc.vpc_id
-#  target_type = "instance"
-#
-#  health_check {
-#    protocol            = "TCP"
-#    port                = "30080"
-#    interval            = 10
-#    timeout             = 5
-#    healthy_threshold   = 2
-#    unhealthy_threshold = 2
-#  }
-#
-#  tags = {
-#    Name = "product-target-group"
-#  }
-#}
-#
-## Descobrir instâncias EC2 do cluster EKS
-#data "aws_instances" "eks_nodes" {
-#  filter {
-#    name   = "tag:eks:cluster-name"
-#    values = [local.name]
-#  }
-#
-#  filter {
-#    name   = "instance-state-name"
-#    values = ["running"]
-#  }
-#}
-#
-## Associar EC2 instances como targets
-#resource "aws_lb_target_group_attachment" "product_targets" {
-#  for_each = toset(data.aws_instances.eks_nodes.ids)
-#
-#  target_group_arn = aws_lb_target_group.product_target_group.arn
-#  target_id        = each.value
-#  port             = 30080
-#}
-#
-## Listener TCP:80 do NLB
-#resource "aws_lb_listener" "product_listener" {
-#  load_balancer_arn = aws_lb.product_nlb.arn
-#  port              = 80
-#  protocol          = "TCP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.product_target_group.arn
-#  }
-#}
+# NLB para o Ingress Controller
+resource "aws_lb" "ingress_nlb" {
+  name               = "tech-challenge-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  security_groups    = [aws_security_group.ingress_nlb_sg.id]
+  subnets            = module.vpc.private_subnets
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "tech-challenge-nlb"
+  }
+}
+
+# Target Group para o Ingress Controller
+resource "aws_lb_target_group" "ingress_target_group" {
+  name        = "ingress-target-group"
+  protocol    = "TCP"
+  port        = 30080
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+
+  health_check {
+    protocol            = "TCP"
+    port                = "30080"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "ingress-target-group"
+  }
+}
+
+# Listener TCP:80 do NLB
+resource "aws_lb_listener" "ingress_listener" {
+  load_balancer_arn = aws_lb.ingress_nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ingress_target_group.arn
+  }
+}
+
+# Associação do Node do Ingress como Target do Target Group
+resource "aws_lb_target_group_attachment" "ingress_target" {
+  target_group_arn = aws_lb_target_group.ingress_target_group.arn
+  target_id        = "i-071934f25995a77b6"
+  port             = 30080
+}
